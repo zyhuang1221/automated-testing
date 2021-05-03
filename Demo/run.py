a15299bb5da1c3bef3449ec45273812408388680
mik <1457139077@qq.com>
2021-05-03 18:40:54 +0800

    0503

--- Demo/run.py
+++ Demo/run.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2021/5/2 
 # @Author  : Mik
+import os
 import pytest
 
 if __name__ == '__main__':
     pytest.main()
+    os.system('allure generate --clean ./report/xml -o ./report/html')
+
