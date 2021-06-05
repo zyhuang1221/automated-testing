14b19e5bffdc258d5c2b2fa8ca547b373ee7d380
mik <1457139077@qq.com>
2021-06-05 17:50:29 +0800

    添加失败重跑

--- WebUiTestCases/test_register.py
+++ WebUiTestCases/test_register.py
@@ -26,6 +26,7 @@ class TestZc():
     def teardown_class(self):
         logger.info(f'{TestZc.__name__}测试完成')
 
+    # @pytest.mark.xfail
     @allure.title('用户注册')
     @pytest.mark.parametrize('user,password', userinfo)
     def testcase1(self, browser, base_url, user, password):
@@ -34,33 +35,33 @@ class TestZc():
             主页_page.my_get_url(base_url)
             主页_page.my_click(主页_page.注册)
         with allure.step('输入用户名，点击注册'):
-            注册_page = XO商城注册.ZC(browser, )
+            注册_page = XO商城注册.ZC(browser)
             注册_page.my_import_text(注册_page.用户名, user)
             注册_page.my_import_text(注册_page.设置登录密码, password)
             # 注册_page.my_click(注册_page.注册按钮)
             注册_page.my_submit(注册_page.设置登录密码)
-        with allure.step('断言'):
-            msg1 = 注册_page.my_switch_alert()
-            print(msg1)
-            logger.info(msg1)
+        # with allure.step('断言'):
+        #     msg1 = 注册_page.my_switch_alert()
+            # print(msg1)
+            # logger.info(msg1)
 
-        # with allure.step('注册断言'):
-        #     登录成功_page = XO商城登录成功页面.DLCG(browser,'登录成功')
-        #
-        #
-        #     try:
-        #         text = 登录成功_page.my_get_text(登录成功_page.欢迎)
-        #
-        #     # text='12121'
-        #     # assert '欢迎来到' in text
-        #         if '欢迎来到' in text:
-        #             logger.info('注册成功')
-        #             登录成功_page.my_click(登录成功_page.退出登录)
-        #
-        #         else:
-        #             raise AssertionError('注册失败')
-        #     except Exception as e:
-        #         raise e
+        with allure.step('注册断言'):
+            登录成功_page = XO商城登录成功页面.DLCG(browser)
+
+
+            try:
+                text = 登录成功_page.my_get_text(登录成功_page.欢迎)
+
+            # text='12121'
+            # assert '欢迎来到' in text
+                if '欢迎来到' in text:
+                    logger.info('注册成功')
+                    登录成功_page.my_click(登录成功_page.退出登录)
+
+                else:
+                    raise AssertionError('注册失败')
+            except Exception as e:
+                raise e
         sleep(3)
 
 # zc=TestZc()
